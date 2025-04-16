4253065d7bb8bd10d37dfc934d18e919bdec144a
Chiru <hasenorc@gmail.com>
2025-04-17 00:13:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -365,7 +365,7 @@ async def account_login(bot: Client, m: Message):
     user_id = str(m.from_user.id)
     channels = read_channels_data()
     subscription_data = read_subscription_data()
-    if not any(user[0] == user_id for user in subscription_data) or channels:
+    if not any(user[0] == user_id for user in subscription_data) or user_id in channels:
         await m.reply_text("❌ You are not a premium user. Please upgrade your subscription! 💎")
         return          
     editable = await m.reply_text("**Please Send TXT file for download**")
